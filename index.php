47775de4743fb0e0a0332a75b765af52ed5ee796
Horatio <hs@horatio.cn>
2022-08-20 02:28:32 +0800

    Add Ver.1

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no">
+    <title>唯蟹Team - Xies' Group</title>
+    <meta name="description"
+        content="唯蟹Team - Xies' Group - 屑们">
+    <meta name="keywords" content="Xies' Group,Xies Group,xies,屑们,唯蟹Team,唯蟹,漉鲸,Lujing,小涵,Horatio,橙妹妹,WangJack">
+    <link rel="stylesheet" type="text/css" href="./css/FiraCode.css">
+    <link rel="stylesheet" type="text/css" href="./css/xies.css">
+    <link rel="shortcut icon" href="https://p.443.fyi/images/2021/10/20/74e0d35d22a759a00ac590df322972b7.jpg">
+</head>
+<body background="https://p.443.fyi/images/2021/10/20/QQ20211020-0.jpg">
+    <div id="box">
+        <!-- 资料卡片 -->
+        <div class="meBox">
+            <!-- 图标 -->
+            <img src="https://p.443.fyi/images/2021/10/20/74e0d35d22a759a00ac590df322972b7.jpg" alt="Logo" height="200" width="200">
+            <!-- 介绍 -->
+                <div class="fg"></div>
+            <div class="meBox-text">
+                <p>唯蟹Team</p>
+                <p>Xies' Group</p>
+                <br>
+                <p>通过技术丰富互联网用户的生活</p>
+                <br>
+                <p>连接你我，共生未来</p>
+                <br>
+            </div>
+        </div>
+        <!-- 伪终端介绍 -->
+        <div id="cmdBox">
+            <!-- 第一个终端 -->
+            <div class="cmd">
+                <!-- 三个按钮 -->
+                <div class="click">
+                    <div class="red"></div>
+                    <div class="yellow"></div>
+                    <div class="green"></div>
+                </div>
+                <!-- 顶部标题 -->
+                <div class="title">
+                    <span>Xies' Group - bash</span>
+                </div>
+                <!-- 终端内文字 -->
+                <div class="cmdText">
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">./tianqi.sh</span>
+                    <br />
+                    <iframe scrolling="no" src="https://tianqiapi.com/api.php?style=tu&skin=pitaya" frameborder="0" width="350" height="24" allowtransparency="true"></iframe>
+                    <br />
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">cat ./us.txt</span>
+                    <p>昵称：<a href="http://wj0.top" target="_blank" title="屑的博客">屑Jack</a></p>
+                    <p>性别：男</p>
+                    <p>地址：河南，中国</p>
+                    <p>邮箱：<a href="mailto:wangjack@mail.wj0.top">wangjack@mail.wj0.top</a></p>
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">sudo rm -rf ./过去的我们/*</span>
+                    <p>sudo: 请输入密码: </p>
+                    <p>bash: 已删除</p>
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                </div>
+            </div>
+            <!-- 第二个终端 -->
+            <div class="cmd cmd2">
+                <!-- 三个按钮 -->
+                <div class="click">
+                    <div class="red"></div>
+                    <div class="yellow"></div>
+                    <div class="green"></div>
+                </div>
+                <!-- 顶部标题 -->
+                <div class="title">
+                    <span>Xies' Group - bash</span>
+                </div>
+                <!-- 终端内文字 -->
+                <div class="cmdText">
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">./links.sh</span>
+                    <p>我们的站点：</p>
+                    <ul class="ul">
+                        <li><a href="https://t.443.fyi">Tools</a></li>
+<br />
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">cat ./us.txt</span>
+                    <br>
+                        <p>昵称：<a href="http://horatio.cn/" target="_blank" title="一个沙雕网友的个人博客">Horatio</a></p>
+                    <p>性别：男</p>
+                    <p>地址：贵州，中国</p>
+                    <p>邮箱：<a href="mailto:Horatio@xies.group">Horatio@xies.group</a></p>
+                    <br>
+                    <p>昵称：<a href="http://blog.lujing0.com/" target="_blank" title="漉鲸的Blog">漉鲸</a></p>
+                    <p>性别：男</p>
+                    <p>地址：江西，中国</p>
+                    <p>邮箱：<a href="mailto:lujing0@hotmail.com">lujing0@hotmail.com</a></p>
+                    </ul>
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">./yiyan.sh</span>
+                    <p class="hito">恰沐春风共同游，终只叹，木已舟。</p>
+                    <span style="color: rgb(0, 190, 0);">root@114514</span>
+                    <span style="color: blue;">~</span>
+                    <span style="color: rgb(39, 39, 39);">./ip.sh</span>
+                    <p class="getip"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- 页脚 -->
+    <div id="footer">
+        <p>© Xies' Group</p>
+    </div>
+    <script src="https://v1.hitokoto.cn/?encode=js&select=.hito" defer></script>
+    <script src="https://pv.sohu.com/cityjson?ie=utf-8"></script>
+    <script type="text/javascript">(function getip(selector){var ip="你的 IP: "+returnCitySN["cip"];var ele=document.querySelector(selector);Array.isArray(ele)?ele[0].innerText=ip:ele.innerText=ip;})('.getip')</script>
+</body>
+
+</html>
